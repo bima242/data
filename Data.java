13759b5015f4d732dc836052b949fb5f26fff403
bima242 <arya_tantular_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:55 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+public class Data {
+
+    public static void main(String[] args) {
+
+        String nama = "bima";
+        int usia = 15;
+        String Alamat = "Blok A No 16 jalan danau ranau 12";
+        double kelas = 10.7;
+
+        System.out.println("namasaya : " + nama); 
+        System.out.println("Usia :" + usia + " tahun ");
+ System.out.println("Alamat : " + Alamat);
+ System.out.println("Kelas : " + kelas);
+    }
+}
